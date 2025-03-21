a78971c047d7e2df6041b5568f40257d5a4e09d8
James Garbutt <43081j@users.noreply.github.com>
2025-03-21 17:43:33 +0000

    fix: simplify `each` types to infer consts
    Simplifies the types for `each` to use `const` type parameters for
    inference.
    
    This means the following now infers correctly:
    
    ```ts
    each(['foo', 'bar'])('', (str) => {
      str; // 'foo' | 'bar'
    });
    
    each([
      ['foo0', 'foo1'],
      ['bar0', 'bar1']
    ])('', (param0, param1) => {
      param0; // 'foo0' | 'bar0'
      param1; // 'foo1' | 'bar1'
    });
    ```

--- packages/runner/src/types/tasks.ts
+++ packages/runner/src/types/tasks.ts
@@ -268,68 +268,30 @@ export type TestFunction<ExtraContext = object> = (
   context: TestContext & ExtraContext
 ) => Awaitable<any> | void
 
-// jest's ExtractEachCallbackArgs
-type ExtractEachCallbackArgs<T extends ReadonlyArray<any>> = {
-  1: [T[0]]
-  2: [T[0], T[1]]
-  3: [T[0], T[1], T[2]]
-  4: [T[0], T[1], T[2], T[3]]
-  5: [T[0], T[1], T[2], T[3], T[4]]
-  6: [T[0], T[1], T[2], T[3], T[4], T[5]]
-  7: [T[0], T[1], T[2], T[3], T[4], T[5], T[6]]
-  8: [T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7]]
-  9: [T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8]]
-  10: [T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8], T[9]]
-  fallback: Array<T extends ReadonlyArray<infer U> ? U : any>
-}[T extends Readonly<[any]>
-  ? 1
-  : T extends Readonly<[any, any]>
-    ? 2
-    : T extends Readonly<[any, any, any]>
-      ? 3
-      : T extends Readonly<[any, any, any, any]>
-        ? 4
-        : T extends Readonly<[any, any, any, any, any]>
-          ? 5
-          : T extends Readonly<[any, any, any, any, any, any]>
-            ? 6
-            : T extends Readonly<[any, any, any, any, any, any, any]>
-              ? 7
-              : T extends Readonly<[any, any, any, any, any, any, any, any]>
-                ? 8
-                : T extends Readonly<[any, any, any, any, any, any, any, any, any]>
-                  ? 9
-                  : T extends Readonly<[any, any, any, any, any, any, any, any, any, any]>
-                    ? 10
-                    : 'fallback']
-
-interface EachFunctionReturn<T extends any[]> {
+interface EachFunctionReturn<T extends readonly any[]> {
   /**
    * @deprecated Use options as the second argument instead
    */
   (
     name: string | Function,
-    fn: (...args: T) => Awaitable<void>,
+    fn: (...args: [...T]) => Awaitable<void>,
     options: TestCollectorOptions
   ): void
   (
     name: string | Function,
-    fn: (...args: T) => Awaitable<void>,
+    fn: (...args: [...T]) => Awaitable<void>,
     options?: number | TestCollectorOptions
   ): void
   (
     name: string | Function,
     options: TestCollectorOptions,
-    fn: (...args: T) => Awaitable<void>
+    fn: (...args: [...T]) => Awaitable<void>
   ): void
 }
 
 interface TestEachFunction {
-  <T extends any[] | [any]>(cases: ReadonlyArray<T>): EachFunctionReturn<T>
-  <T extends ReadonlyArray<any>>(cases: ReadonlyArray<T>): EachFunctionReturn<
-    ExtractEachCallbackArgs<T>
-  >
-  <T>(cases: ReadonlyArray<T>): EachFunctionReturn<T[]>
+  <const T extends readonly any[]>(cases: readonly T[]): EachFunctionReturn<T>
+  <const T>(cases: readonly T[]): EachFunctionReturn<[T]>
   (...args: [TemplateStringsArray, ...any]): EachFunctionReturn<any[]>
 }
 
